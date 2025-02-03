52a5323d25d73a90ea729c301a1ca4f447663d3c
luka574 <trapaidzeluka034@gmail.com>
2025-02-03 23:40:37 +0400

    first commit

---
+++ day01/classwork/lesson1.py
@@ -0,0 +1,34 @@
+name = "luka"
+#name aris cvladi
+#= aris cvladistvis mnishvnelobis mamnichebeli simbolo
+# "luka" aris cvladistvismnishvneloba
+
+surname = "trapaidze"
+
+# print(name)
+#print funqcias gadaecema ekranze gamosatani obiekti
+
+name="luka" #es aris str (string) tipis cvladi
+age = 22 # es aris int (integer) mteli ricxvi
+height = 184.5 #es aris float tipis cvladi (atwiladi)
+#booLean (booL) tipis cvladi
+
+knows_programming =True #true an false
+is_ugly =False
+
+isugly = False
+
+
+print(name + " " + surname)
+
+#stringi aris brchyalebshi mokceuli simboloebi
+#print(name + age)
+
+print(type(17))
+print(type(name))
+print(type(surname))
+print(type(178.5))
+print(type(knows_programming))
+
+print(name + str(age))
+
---
+++ day01/homework/homework1.py
@@ -0,0 +1,7 @@
+name = "luka"
+surname = "trapaidze"
+age = 17
+place = "tbilisi"
+height = 178.5
+works = False
+print (name + " " +surname + " " +str(age)+ " " + place + " " +str (height))
