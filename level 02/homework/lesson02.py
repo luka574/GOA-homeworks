a1483de945e62764bcdbde3ddce03cc4d6781b22
luka574 <trapaidzeluka034@gmail.com>
2025-02-22 13:14:51 +0400

    commit

---
+++ level 02/homework/lesson02.py
@@ -0,0 +1,39 @@
+#data type მონაცემთა ტიპები integer string float boolean 
+#ინტეჯერი არის მთელი რიცხვები  (10)
+#სტრინგი არის ნებისმიერი რამ რაც ბრჭყალებში არის   ("luka")
+#ფლოუთი არის ათწილადი რიცხვები (10.5)
+#ბულიანი არის true da false
+
+
+# გააკეთეთ ორ რიცხვს შორის თითოეულ მათემატიკურ ოპერაციაზე 5-5 მაგალითი (+,-,*,/) და დაპრინტეთ
+print (10+5)
+print (10-5)
+print (10*5)
+print (10/5)
+
+# ცვლადები სადაც იქნება თქვენი სახელი, თქვენი გვარი, თქვენი ასაკი და დაბეჭდეთ ერთ დიდ წინადადებაში
+name="luka"
+surname="trapaidze"
+age=17
+print (name+surname+str(age))
+
+
+# შექმენი 9 ცვლადი, 3 ცვლადი იყოს Str, 3 ცვლადი int, 3 ცვლადი float, და შემდეგ გამოიტანე ტერმინალში 9-ვე ცვლადი print-ის მეშვეობით.
+name="nika"
+surname="shavadze"
+city="tbilisi"
+age=17
+height=178
+weight=85
+number1=5.5
+number2=10.5
+number3=15.5
+print (name)
+print (surname)
+print (city)
+print (age)
+print (height)
+print (weight)
+print (number1)
+print (number2)
+print (number3)
---
+++ level 02/homework/lesson022.py
@@ -0,0 +1,19 @@
+name="luka"
+age=17
+height=177.5
+surname=True
+
+print (name+str(age)+str(height))
+
+age=20
+weight=65
+print (str(age)+str(weight))
+
+name="luka"
+surname="trapaidze"
+city="tbilisi"
+
+print (name+surname+city)
+
+number1="30"
+print (int(number1))
